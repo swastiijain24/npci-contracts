75dd5e7ee87d0b40698e645bdcbf5e5e80bd706c
Swasti <swastiijain@gmail.com>
2026-04-09 14:55:41 +0530

    npci-shared

---
+++ constants/constant.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+    TopicPaymentRequest  = "payment.request.v1"
+    TopicBankInstruction = "bank.instruction.v1"
+    TopicBankOutcome     = "bank.outcome.v1"
+    TopicPaymentResponse = "payment.response.v1"
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/swastiijain24/npci-shared
+
+go 1.26.1
