f89256eea32864f05750d30adabf5251e60254f4
Swasti <swastiijain@gmail.com>
2026-04-10 23:18:10 +0530

    protocol buffer

--- constants/constant.go
+++
@@ -1,8 +0,0 @@
-package constants
-
-const (
-    TopicPaymentRequest  = "payment.request.v1"
-    TopicBankInstruction = "bank.instruction.v1"
-    TopicBankOutcome     = "bank.outcome.v1"
-    TopicPaymentResponse = "payment.response.v1"
-)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/swastiijain24/npci-shared
+module github.com/swastiijain24/npci-contracts
 
 go 1.26.1
+
+require google.golang.org/protobuf v1.36.11
